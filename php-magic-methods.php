7abbd093e192740c827b9abb1aaaef53ea09e9cd
Faisal Haroon <faisalharoon@Mehnoors-MacBook-Pro.local>
2026-08-19 15:28:29 +0500

    __destruct method execution understanding uncommented

--- php-magic-methods.php
+++ php-magic-methods.php
@@ -36,50 +36,50 @@ declare(strict_types=1);
 // echo "Total amount to be cahrged including fee: {$total}\n";
 
 
-// class AuditLogger
+class AuditLogger
 
-//  {
+ {
 
-//     private $fileStream;
+    private $fileStream;
 
-//     public function __construct(
+    public function __construct(
 
-//         public string $logFile
-//     ){
+        public string $logFile
+    ){
 
-//       $this->fileStream = fopen($this->logFile, 'a');
-//       echo "1. [CONSTRUCT] Log file stream opened for writing: {$this->logFile}\n";
-//     }
+      $this->fileStream = fopen($this->logFile, 'a');
+      echo "1. [CONSTRUCT] Log file stream opened for writing: {$this->logFile}\n";
+    }
 
-//     public function writeLog(string $event) : void {
+    public function writeLog(string $event) : void {
 
-//          if($this->fileStream) {
-//             fwrite($this->fileStream, date('Y-m-d H:i:s') . " - {$event}\n");
-//          } else {
-//             throw new RuntimeException("File stream is not available.");
-//          }
-//     }
+         if($this->fileStream) {
+            fwrite($this->fileStream, date('Y-m-d H:i:s') . " - {$event}\n");
+         } else {
+            throw new RuntimeException("File stream is not available.");
+         }
+    }
 
-//     // object memory se remove hote hi autorun hoga
+    // object memory se remove hote hi autorun hoga
 
-//     public function __destruct()
-//     {
-//         if (is_resource($this->fileStream)) {
-//             fclose($this->fileStream);
-//             echo "3. [DESTRUCT] File stream closed safely. Zero resource leaks!\n";
-//         }
-//     }
+    public function __destruct()
+    {
+        if (is_resource($this->fileStream)) {
+            fclose($this->fileStream);
+            echo "3. [DESTRUCT] File stream closed safely. Zero resource leaks!\n";
+        }
+    }
 
 
-// }
+}
 
-// function executeLoggingProcess(): void
-// {
-//     $logger = new AuditLogger('system_audit.log');
-//     $logger->writeLog("User #102 changed password");
-//     echo "--- End of function scope ---\n";
-// }
+function executeLoggingProcess(): void
+{
+    $logger = new AuditLogger('system_audit.log');
+    $logger->writeLog("User #102 changed password");
+    echo "--- End of function scope ---\n";
+}
 
-// executeLoggingProcess();
+executeLoggingProcess();
 
-// echo "4. Script execution completely finished.\n";
+echo "4. Script execution completely finished.\n";
